be26a31ab3578a94c6cb159f61fe66ad3826ab98
Harry Li <haoli1989@gmail.com>
2013-07-12 05:21:35 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var buffer = new buffer();
-	response.send(buffer.toString("utf-8", fs.readFileSync("index.html", "utf-8")));
+  var buffer = new buffer ();
+	response.send(buffer.toString("utc 8", fs.readFileSync("index.html")))});
 });
 
 var port = process.env.PORT || 5000;
