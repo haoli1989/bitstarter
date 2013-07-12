0618d8ed025ce4c7632683e007f2d12232ec7184
Harry Li <haoli1989@gmail.com>
2013-07-12 05:04:32 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,5 +1,14 @@
 var express = require('express');
 
+var fs = require('fs');
+var FILE_DEFAULT = "index.html";
+
+var message() = function (file) {
+file = 'index.html';
+var buffer = new buffer(fs.readFileSync(file));
+return (buffer.toString());
+}
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
